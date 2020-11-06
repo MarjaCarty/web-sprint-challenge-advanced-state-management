14c3ec8a6f4aecaae1c4c057ab55744ec87a2f30
MarjaCarty <marja.d.carty@gmail.com>
2020-11-06 10:51:28 -0600

    Construct form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -8,7 +8,52 @@ const initialFormValues = {
 
 function SmurfForm() {
   const [formValues, setFormValues] = useState(initialFormValues);
-  return <div></div>;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormValues(initialFormValues);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formValues.name}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Age:
+        <input
+          type="text"
+          name="age"
+          value={formValues.age}
+          onChange={handleChange}
+        />
+      </label>
+
+      <label>
+        Height:
+        <input
+          type="text"
+          name="height"
+          value={formValues.height}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="submit">Add Smurf</button>
+    </form>
+  );
 }
 
 export default SmurfForm;
